8a9d39e4c28efc3baacac1e5946705e34c302c6b
Laust <laust.bonnesen@mail.com>
2021-04-12 11:40:40 +0200

    halløj halløj

--- src/main/java/kea_pal_gruppe_3/mini_projekt/controllers/UserController.java
+++ src/main/java/kea_pal_gruppe_3/mini_projekt/controllers/UserController.java
@@ -1,4 +1,14 @@
 package kea_pal_gruppe_3.mini_projekt.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/")
+    public String start() {
+        return "";
+    }
+
 }
