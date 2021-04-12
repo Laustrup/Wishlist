5709841c62309c3e977793a275edd74d5554078b
Patrick <skovskiver@gmail.com>
2021-04-12 11:36:26 +0200

    added Wish class

---
+++ src/main/java/kea_pal_gruppe_3/mini_projekt/models/Wish.java
@@ -0,0 +1,15 @@
+package kea_pal_gruppe_3.mini_projekt.models;
+
+public class Wish {
+
+    String wish;
+    String url;
+
+    public Wish (String wish, String url){
+
+        this.wish = wish;
+        this.url = url;
+    }
+
+
+}
