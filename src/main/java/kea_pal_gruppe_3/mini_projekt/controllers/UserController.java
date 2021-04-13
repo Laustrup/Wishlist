60d5337663a623528f3cd64f316d726b01e17685
Laust <laust.bonnesen@mail.com>
2021-04-13 14:25:55 +0200

    Småting

--- src/main/java/kea_pal_gruppe_3/mini_projekt/controllers/UserController.java
+++ src/main/java/kea_pal_gruppe_3/mini_projekt/controllers/UserController.java
@@ -1,14 +1,8 @@
 package kea_pal_gruppe_3.mini_projekt.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-
-    @GetMapping("/")
-    public String start() {
-        return "";
-    }
 
 }
