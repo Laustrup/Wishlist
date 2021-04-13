da74ace023ba5a590f1ee2d092ead1841578ca86
Anton <anto8214@stud.kea.dk>
2021-04-13 10:31:27 +0200

    Updated 'User.java' and added folders for HTML

--- src/main/java/kea_pal_gruppe_3/mini_projekt/models/User.java
+++ src/main/java/kea_pal_gruppe_3/mini_projekt/models/User.java
@@ -1,4 +1,15 @@
 package kea_pal_gruppe_3.mini_projekt.models;
 
 public class User {
+
+    final String ID;
+    String name;
+    String password;
+
+    public User(String ID, String name, String password) {
+        this.ID = ID;
+        this.name = name;
+        this.password = password;
+
+    }
 }
