c53bcc6e6b18a18eee66b1f38e212bf3a312489f
Anton <anto8214@stud.kea.dk>
2021-04-13 13:17:38 +0200

    Beautification

--- src/main/java/kea_pal_gruppe_3/mini_projekt/models/User.java
+++ src/main/java/kea_pal_gruppe_3/mini_projekt/models/User.java
@@ -2,9 +2,9 @@ package kea_pal_gruppe_3.mini_projekt.models;
 
 public class User {
 
-    final String ID;
-    String name;
-    String password;
+    private final String ID;
+    private String name;
+    private String password;
 
     public User(String ID, String name, String password) {
         this.ID = ID;
