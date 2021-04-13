ac30fd785e271efbad6d23bd4b48d20582570ff3
Patrick <skovskiver@gmail.com>
2021-04-13 14:28:25 +0200

    deleted method in userController

--- src/main/java/kea_pal_gruppe_3/mini_projekt/controllers/UserController.java
+++ src/main/java/kea_pal_gruppe_3/mini_projekt/controllers/UserController.java
@@ -1,6 +1,7 @@
 package kea_pal_gruppe_3.mini_projekt.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
